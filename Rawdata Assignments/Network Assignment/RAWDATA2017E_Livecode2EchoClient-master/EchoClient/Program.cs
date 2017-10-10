5e6efbeea3200f25c95fe0afd0e73f8a405ec2a3
Rune Barrett <runebarrett@gmail.com>
2017-10-10 14:21:09 +0200

    new version from henrik + add zip's

---
+++ Rawdata Assignments/Network Assignment/RAWDATA2017E_Livecode2EchoClient-master/EchoClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 5000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            var client = new TcpClient();
+
+            client.Connect(localAddr, port);
+
+            var strm = client.GetStream();
+            var request = "Hello";
+            Console.WriteLine($"Request: {request}");
+            var data = Encoding.UTF8.GetBytes(request);
+
+            strm.Write(data, 0, data.Length);
+
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            var bytesRead = strm.Read(buffer, 0, buffer.Length);
+
+            var response = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine($"Response: {response.Trim('\0')}");
+
+            strm.Close();
+            client.Dispose();
+
+
+        }
+    }
+}
