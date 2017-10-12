fcb9b566455c8f67efe3130bc2dd3a1a3d640fcb
Rune Barrett <runebarrett@gmail.com>
2017-10-12 21:45:50 +0200

    pass all 28 tests. constraints and api tests seperated on server

--- Rawdata Assignments/Network Assignment/Server_1/RAWDATA2017E_Livecode2-master/EchoServer/DataAPI.cs
+++ Rawdata Assignments/Network Assignment/Server_1/RAWDATA2017E_Livecode2-master/EchoServer/DataAPI.cs
@@ -10,8 +10,36 @@ namespace Server1
         public static void InitDataModel() => _model = new DataModel();
 
         public static void Create(Server.RequestObj requestObj, ref Server.Response response)
-        {   
-            var passed = CheckPath(requestObj, response);
+        {
+            //------------------ Constraints ----------------
+            
+            var violatedConstraint = false;
+            //Missing body
+            if (requestObj.Body == null)
+            {
+                response.Status += "missing body, ";
+                violatedConstraint = true;
+            }
+            //Missing path/resource
+            if (requestObj.Path == null)
+            {
+                response.Status += "missing resource, ";
+                violatedConstraint = true;
+            }
+
+            if (violatedConstraint) return;
+            //If id is already present
+            if (_model.Retrieve(requestObj.Path) != null)
+            {
+                response.Status = response.Status = "4 Bad Request";
+                return;
+            }
+
+            var b = _model.Create(requestObj.Path, JsonConvert.DeserializeObject<Category>(requestObj.Body).Name);
+            response.Body = JsonConvert.SerializeObject(b);
+
+            //if (!PathIsValid(requestObj, response)) return;
+            /*var passed = CheckPath(requestObj, response);
             if (_model.Retrieve(requestObj.Path) != null)
             {
                 //The path already exists
@@ -25,7 +53,7 @@ namespace Server1
             if (!passed) return;
 
             var b =_model.Create(requestObj.Path, JsonConvert.DeserializeObject<Category>(requestObj.Body).Name);
-            response.Body = JsonConvert.SerializeObject(b);
+            response.Body = JsonConvert.SerializeObject(b);*/
         }
 
 
@@ -41,7 +69,7 @@ namespace Server1
 
             //------------------ API tests ------------------
             //First check if it is a readAll call
-            if (requestObj.Path == "/api/categories")
+            if (requestObj.Path == "/api/categories") //(slightly hacky i suppose?)
             {
                 response.Status = "1 Ok";
                 response.Body = JsonConvert.SerializeObject(_model.ReadAll());
@@ -49,20 +77,21 @@ namespace Server1
             }
 
             //Else, check if path is valid
-            if (!requestObj.Path.Contains("/api/categories") || !requestObj.Path.Any(char.IsDigit)) //Must be valid eg. contain "/api/categories", and must contain a number (the id)
-            {
-                response.Status = "4 Bad Request";
-                return;
-            }
+            if (!PathIsValid(requestObj, response)) return;
 
-            //If not, check if exists
-            if (Exists(requestObj, response))
+            //Else, if exists, retrieve and return it
+            var category = _model.Retrieve(requestObj.Path);
+            if (category != null)
             {
                 response.Status = "1 Ok";
-                response.Body = JsonConvert.SerializeObject(_model.Retrieve(requestObj.Path));
+                response.Body = JsonConvert.SerializeObject(category);
+            }
+            else
+            {
+                response.Status += "5 not found, ";
             }
 
-            //If it does, retrieve and return it
+            //If it does, 
 
             /*if (requestObj.Path == "/api/categories") //read all (slightly hacky, i know) ;)
             {
@@ -83,7 +112,46 @@ namespace Server1
 
         public static void Update(Server.RequestObj requestObj, ref Server.Response response)
         {
-            var passed = CheckPath(requestObj, response);
+            //------------------ Constraints ----------------
+            var violatedConstraint = false;
+
+            //Check if path is present
+            if (string.IsNullOrEmpty(requestObj.Path)) //If there is nothing in the path, add 'missing resource'
+            {
+                response.Status += "missing resource, ";
+                violatedConstraint = true;
+            }
+            //Check for missing body
+            if (requestObj.Body == null)
+            {
+                response.Status += "missing body, ";
+                violatedConstraint = true;
+            }
+            //Check for illegal body
+            else if (!requestObj.Body.StartsWith("{"))
+            {
+                response.Status += "illegal body, ";
+                violatedConstraint = true;
+            }
+            if(violatedConstraint) return;
+
+            //------------------ API tests ------------------
+            //Check if path is valid
+            if (!PathIsValid(requestObj, response)) return; //actually a constraint but returns only "4 Bad Request"
+
+            //Check if exists in "database" 
+            if (_model.Retrieve(requestObj.Path) == null)
+            {
+                response.Status += "5 not found, ";
+                return;
+            }
+
+            //Update category
+            var b = _model.UpdateName(requestObj.Path, JsonConvert.DeserializeObject<Category>(requestObj.Body).Name);
+            response.Status += "3 updated";
+            response.Body = JsonConvert.SerializeObject(b);
+
+            /*var passed = CheckPath(requestObj, response);
             if (!Exists(requestObj, response)) passed = false;
             if (!CheckBody(requestObj, response)) passed = false;
             if (!CheckPathForId(requestObj, response)) passed = false;
@@ -91,7 +159,7 @@ namespace Server1
 
             var b = _model.UpdateName(requestObj.Path, JsonConvert.DeserializeObject<Category>(requestObj.Body).Name);
             response.Status += "3 updated";
-            response.Body = JsonConvert.SerializeObject(b);
+            response.Body = JsonConvert.SerializeObject(b);*/
         }
 
         public static void Delete(Server.RequestObj requestObj, ref Server.Response response)
@@ -117,6 +185,17 @@ namespace Server1
 
         ////////// Private methods //////////
 
+
+        private static bool PathIsValid(Server.RequestObj requestObj, Server.Response response)
+        {
+            if (requestObj.Path != null && (!requestObj.Path.Contains("/api/categories") || !requestObj.Path.Any(char.IsDigit))) //Must be valid eg. contain "/api/categories", and must contain a number (the id)
+            {
+                response.Status = "4 Bad Request";
+                return false;
+            }
+            return true;
+        }
+
         private static bool CheckPathForId(Server.RequestObj requestObj, Server.Response response)
         {
             if (response.Status != null && response.Status.ToLower().Contains("5 not found")
@@ -128,13 +207,13 @@ namespace Server1
             return true;
         }
 
-        private static bool Exists(Server.RequestObj requestObj, Server.Response response)
+        /*private static bool Exists(Server.RequestObj requestObj, Server.Response response)
         {
             if (_model.Retrieve(requestObj.Path) != null) return true;
             //if (!response.Status.Contains("Bad Request"))
                 response.Status += "5 not found, ";
             return false;
-        }
+        }*/
 
         private static bool CheckPath(Server.RequestObj requestObj, Server.Response response)
         {
