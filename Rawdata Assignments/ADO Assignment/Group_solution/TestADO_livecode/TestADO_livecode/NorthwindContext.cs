6d420d9e7cf4bde3fee902f32c823112c15dd538
Rune Barrett <runebarrett@gmail.com>
2017-10-17 13:46:00 +0200

    refactor readlines

--- Rawdata Assignments/ADO Assignment/Group_solution/TestADO_livecode/TestADO_livecode/NorthwindContext.cs
+++ Rawdata Assignments/ADO Assignment/Group_solution/TestADO_livecode/TestADO_livecode/NorthwindContext.cs
@@ -14,10 +14,8 @@ namespace TestADO_livecode
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            var lines = File.ReadLines("credentials.txt").ToArray();
-            var username = lines[0];
-            var password = lines[1];
-            optionsBuilder.UseMySql("server=localhost;database=northwind;uid="+username+";pwd="+password);
+            var lines = File.ReadLines("credentials.txt").ToArray(); //Use credential file since we have different passwords on our DB servers
+            optionsBuilder.UseMySql("server=localhost;database=northwind;uid="+ lines[0] + ";pwd="+ lines[1]);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
