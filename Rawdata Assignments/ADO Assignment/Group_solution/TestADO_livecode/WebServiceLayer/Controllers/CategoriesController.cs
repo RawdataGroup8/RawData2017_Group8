c9263ac55f02c605614a1e64354b69f8c9f77bcc
Rune Barrett <runebarrett@gmail.com>
2017-10-23 16:44:50 +0200

    Post works (it was an object, not a json string)

--- Rawdata Assignments/ADO Assignment/Group_solution/TestADO_livecode/WebServiceLayer/Controllers/CategoriesController.cs
+++ Rawdata Assignments/ADO Assignment/Group_solution/TestADO_livecode/WebServiceLayer/Controllers/CategoriesController.cs
@@ -19,46 +19,47 @@ namespace WebServiceLayer.Controllers
 
         // GET: api/Categories
         [HttpGet(Name = "GetCategories")]
-        public string Get()
+        public IActionResult Get()
         {
-            return JsonConvert.SerializeObject(_ds.GetCategories());
+            return Ok(_ds.GetCategories());
         }
 
         // GET: api/Categories/5
         [HttpGet("{id}", Name = "GetCategory")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            return _ds.GetCategory(id) != null ? (IActionResult)Ok(_ds.GetCategory(id)) : NotFound(_ds.GetCategory(id));
+
         }
 
         // POST: api/Categories
         [HttpPost(Name = "AddCategory")]
-        public IActionResult createCategory([FromBody]string content)
+        public IActionResult CreateCategory([FromBody](string, string) content)
         {
-            (string, string) t = ((string, string)) JsonConvert.DeserializeObject(content);
-            if (!(_ds.CreateCategory(t.Item1, t.Item2).Equals(null)))
-            {
-                return Created("created", content);
-            }
-            else { return NotFound(); }
-            //Ok(_ds.CreateCategory(name, value));
+            return Created("created", _ds.CreateCategory(content.Item1, content.Item2));
         }
+
         // PUT: api/Categories/5
         [HttpPut("{id}", Name = "UpdateOrAddCategory")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody](string, string) values)
         {
+            //if (values == null) return NotFound();
+            //var content = ((string, string))JsonConvert.DeserializeObject(values);
+            var res = _ds.UpdateCategory(id, values.Item1, values.Item2);
+            return res ? (IActionResult)Ok() : NotFound();
         }
-        
+
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}", Name = "DeleteCategory")]
         public IActionResult Delete(int id)
-        {            
-            var del = _ds.DeleteCategory(id);
-            if (del)
-            {
-                return Ok(_ds.DeleteCategory(id));
-            }
-            return NotFound(_ds.DeleteCategory(id));
+        {
+            return _ds.DeleteCategory(id) ? (IActionResult)Ok(_ds.DeleteCategory(id)) : NotFound(_ds.DeleteCategory(id));
+        }
+
+        // helpers
+        public T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
         }
     }
 }
