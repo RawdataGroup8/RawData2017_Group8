f84935ef27a18429d18703f524a2cc634866da18
kidane12 <kidun19@yahoo.com>
2017-10-28 23:34:35 +0200

    Dataservice and runner classes

---
+++ Project Portfolio/Project 2/Portfolio subproject 2/DAL/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.DBObjects;
+
+namespace DAL
+{
+    class DataService
+    {
+
+        private readonly StackoverflowContext _db;
+        public DataService()
+        {
+            _db = new StackoverflowContext();
+        }
+
+        public List<User> GetUsers()
+        {
+            return _db.User.ToList();
+        }
+
+
+
+    }
+}
---
+++ Project Portfolio/Project 2/Portfolio subproject 2/DAL/Runner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class Runner
+    {
+        private static void Main(string[] args)
+        {
+            var ds = new DataService();
+           
+
+
+
+
+
+
+
+        }
+
+    }
+}
