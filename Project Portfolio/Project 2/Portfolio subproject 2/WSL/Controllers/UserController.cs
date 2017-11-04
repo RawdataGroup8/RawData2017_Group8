86182e73c7c488728841e8932a8049713c04d3d5
Kristoffer <kristoffercs@gmail.com>
2017-11-04 06:50:32 +0100

    add auto generating links to UserController

--- Project Portfolio/Project 2/Portfolio subproject 2/WSL/Controllers/UserController.cs
+++ Project Portfolio/Project 2/Portfolio subproject 2/WSL/Controllers/UserController.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.DataAnnotations.Internal;
 using DAL;
 using DAL.DBObjects;
+using WSL.Models;
 
 namespace WSL.Controllers
 {
@@ -16,29 +18,84 @@ namespace WSL.Controllers
     public class UserController : Controller
     {
         private readonly IDataService _ds;
+        private readonly IMapper mapper;
         public UserController(IDataService iDataService)
         {
             _ds = iDataService;
+            mapper = CreateMapper();
         }
 
         [HttpGet(Name = nameof(GetUsers))] 
-        public IActionResult GetUsers()
+        public IActionResult GetUsers(int page = 0, int pageSize = 10)
         {
-            return Ok(_ds.GetUsers());
+            CheckPageSize(ref pageSize);
+
+            var total = _ds.GetUsers().Count;
+            var totalPages = GetTotalPages(pageSize, total);
+
+            var data = _ds.GetUsers(page, pageSize)
+                .Select(x => new SimpleReturnModel
+                {
+                    Url = Url.Link(nameof(GetUsers), new {id = x.Userid}),
+                    Name = x.UserName
+                });
+
+            var result = new
+            {
+                Total = total,
+                Pages = totalPages,
+                Page = page,
+                Prev = Link(nameof(GetUsers), page, pageSize, -1, () => page > 0),
+                Next = Link(nameof(GetUsers), page, pageSize, +1, () => page < totalPages - 1),
+                Url = Link(nameof(GetUsers), page, pageSize),
+                Data = data
+            };
+
+            return Ok(result);
         }
 
         //like: "api/User/1"
         [HttpGet("{id}", Name = nameof(GetUser))]
         public IActionResult GetUser(int id)
         {
+//            var user = _ds.GetUser(id);
+//            if (user == null) return NotFound();
+//
+//            var model = mapper.Map<UserReturnModel>(User);
+//            model.Url = Url.Link(nameof(GetUser), new {id = user.Userid});
+//
+//            return Ok(model);
             return Ok(_ds.GetUser(id));
         }
 
+        public IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserReturnModel>()
+                    .ReverseMap();
+            });
 
+            return config.CreateMapper();
+        }
 
+        private string Link(string route, int page, int pageSize, int pageInc = 0, Func<bool> f = null)
+        {
+            if (f == null) return Url.Link(route, new { page, pageSize });
 
+            return f()
+                ? Url.Link(route, new { page = page + pageInc, pageSize })
+                : null;
+        }
 
+        private static int GetTotalPages(int pageSize, int total)
+        {
+            return (int)Math.Ceiling(total / (double)pageSize);
+        }
 
-
+        private static void CheckPageSize(ref int pageSize)
+        {
+            pageSize = pageSize > 50 ? 50 : pageSize;
+        }
     }
 }
---
+++ Project Portfolio/Project 2/Portfolio subproject 2/WSL/Models/UserReturnModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSL.Models
+{
+    public class UserReturnModel : SimpleReturnModel
+    {
+        public string Description { get; set; }
+    }
+}
