f87c349ce18684cdfc960d01b4a91acb61c77ec5
Rune Barrett <runebarrett@gmail.com>
2017-11-07 21:46:08 +0100

    implement GetQuestion in webservice, cleanup, working on questions

--- Project Portfolio/Project 2/Portfolio subproject 2/WSL/Controllers/AbstractController.cs
+++ Project Portfolio/Project 2/Portfolio subproject 2/WSL/Controllers/AbstractController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebServiceLayer.Controllers
@@ -11,17 +8,11 @@ namespace WebServiceLayer.Controllers
         protected string Link(string route, int page, int pageSize, int pageInc = 0, Func<bool> f = null)
         {
             if (f == null) return Url.Link(route, new { page, pageSize });
-
-            return f()
-                ? Url.Link(route, new { page = page + pageInc, pageSize })
-                : null;
+            return f() ? Url.Link(route, new { page = page + pageInc, pageSize }) : null;
         }
 
         protected int GetTotalPages(int pageSize, int total) => (int)Math.Ceiling(total / (double)pageSize);
 
-        protected static void CheckPageSize(ref int pageSize)
-        {
-            pageSize = pageSize > 50 ? 50 : pageSize; //Q: What is 50 all about? :P A: it's just the upperlimet of the pagesize 
-        }
+        protected static void CheckPageSize(ref int pageSize) => pageSize = pageSize > 50 ? 50 : pageSize;
     }
 }
