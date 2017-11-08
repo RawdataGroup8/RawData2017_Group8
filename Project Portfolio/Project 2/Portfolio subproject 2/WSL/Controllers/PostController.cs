5955e20bf51ebbf96b014beefb71f5a21ffc6e55
Kristoffer <kristoffercs@gmail.com>
2017-11-08 14:02:30 +0100

    made getpost return "This is an answer to the question: " + "title of the question" if the post is an answer

--- Project Portfolio/Project 2/Portfolio subproject 2/WSL/Controllers/PostController.cs
+++ Project Portfolio/Project 2/Portfolio subproject 2/WSL/Controllers/PostController.cs
@@ -14,21 +14,46 @@ namespace WebServiceLayer.Controllers
         [HttpGet("{id}", Name = nameof(GetPost))]
         public IActionResult GetPost(int id)
         {
-            var result = new
+            PostDTO data;
+            if (_ds.GetPost(id).PostId == 1)
             {
-                //maybe add an if statement so it only runes this code if the post has a title
-                Data = new PostDTO
+                data = new PostDTO
                 {
-                    Url = Url.Link(nameof(GetPost), new { id }),
+                    Url = Url.Link(nameof(GetPost), new {id}),
                     Title = _ds.GetPost(id).Title,
-                    Author = _ds.GetUser(_ds.GetPost(id).OwnerUserId).UserName, // becaus _ds.GetPost(id).User.UserName dosent work... for some reason
-                    AuthorUrl = Url.Link(nameof(UserController.GetUser), new { id =_ds.GetPost(id).OwnerUserId }),
+                    Author =
+                        _ds.GetUser(_ds.GetPost(id).OwnerUserId)
+                            .UserName, // becaus _ds.GetPost(id).User.UserName dosent work... for some reason
+                    AuthorUrl = Url.Link(nameof(UserController.GetUser), new {id = _ds.GetPost(id).OwnerUserId}),
                     CreationDate = _ds.GetPost(id).CreationDate,
                     Score = _ds.GetPost(id).Score,
                     Body = _ds.GetPost(id).Body,
                     Comments = _ds.GetPost(id).Comments,
                     PostTags = _ds.GetPost(id).PostTags
-                }
+                };
+            }
+            else
+            {
+                data = new PostDTO
+                {
+                    Url = Url.Link(nameof(GetPost), new { id }),
+                    Title = "This is an answer to the question: " + _ds.GetPost(_ds.GetAnswer(id).Parentid).Title, //maybe we should include a link to it
+                    Author =
+                        _ds.GetUser(_ds.GetPost(id).OwnerUserId)
+                            .UserName, // becaus _ds.GetPost(id).User.UserName dosent work... for some reason
+                    AuthorUrl = Url.Link(nameof(UserController.GetUser), new { id = _ds.GetPost(id).OwnerUserId }),
+                    CreationDate = _ds.GetPost(id).CreationDate,
+                    Score = _ds.GetPost(id).Score,
+                    Body = _ds.GetPost(id).Body,
+                    Comments = _ds.GetPost(id).Comments,
+                    PostTags = _ds.GetPost(id).PostTags
+                };
+            }
+
+            var result = new
+            {
+                //maybe add an if statement so it only runes this code if the post has a title
+                Data = data
             };
             return Ok(result);
         }
