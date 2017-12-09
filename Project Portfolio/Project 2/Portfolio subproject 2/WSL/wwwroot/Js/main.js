92c4930fee165091e739949a09d096ee91d4f277
kidane12 <kidun19@yahoo.com>
2017-12-09 19:57:56 +0100

    Require.config file

--- Project Portfolio/Project 2/Portfolio subproject 2/WSL/wwwroot/Js/main.js
+++ Project Portfolio/Project 2/Portfolio subproject 2/WSL/wwwroot/Js/main.js
@@ -1,1 +1,9 @@
-﻿
+﻿require.config({
+    baseUrl: "js",
+    paths: {
+        jquery: "../lib/jquery/dist/jquery.min",
+        knockout: "../lib/knockout/dist/knockout",
+        text: "../lib/text/text",
+        jqcloud: '../lib/jqcloud2/dist/jqcloud.min'
+    }
+});
