4bb70d75759cd8080beda70a8dce867710018b05
Kristoffer <kristoffercs@gmail.com>
2017-12-11 22:35:53 +0100

    Add folder for services and a todo note

--- Project Portfolio/Kristoffer's atempt at implementing spa/Portfolio subproject 2/WSL/wwwroot/js/components/c3/c3.js
+++ Project Portfolio/Kristoffer's atempt at implementing spa/Portfolio subproject 2/WSL/wwwroot/js/components/c3/c3.js
@@ -1,6 +1,6 @@
 ﻿define(['knockout'], function(ko) {
     return function (params) {
-        var title = ko.observable("Component 3");
+        var title = ko.observable("Component 3"); // the listing should be done like in 4.2
         return {
             title
        };
