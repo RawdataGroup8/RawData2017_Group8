2f1750ed03075431f0308e63798b90d9832051c7
kidane12 <kidun19@yahoo.com>
2017-12-12 18:49:31 +0100

    We may not need it but now both the WebserviceLayers are working(Weblayer and Webservicelayer (using bower)).

--- Project Portfolio/Project 2/Portfolio subproject 2/WSL/wwwroot/js/services/postman.js
+++ Project Portfolio/Project 2/Portfolio subproject 2/WSL/wwwroot/js/services/postman.js
@@ -1,4 +1,4 @@
-﻿define([], function() {
+﻿define([], function () {
 
     var subscribers = [];
 
@@ -6,24 +6,23 @@
         var subscriber = { event, callback };
         subscribers.push(subscriber);
 
-        return function() {
+        return function () {
             var index = subscribers.indexOf(subscriber);
             subscribers.slice(index, 1);
         };
 
     };
 
-    var publish = (event, payload) => 
-    {
+    var publish = (event, payload) => {
         subscribers.forEach(s => {
             if (s.event === event) s.callback(payload);
         });
     }
 
     var events =
-    {
-        changeView: "changeView"
-    };
+        {
+            changeView: "changeView"
+        };
 
     return {
         subscribe,
