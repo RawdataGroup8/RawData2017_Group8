b5bd5e8ab1e27d4ca7335b781d04b5c186ca9c4d
Rune Barrett <runebarrett@gmail.com>
2017-12-14 23:10:57 +0100

    temporarily include ex 4_3  to the solution for testing

---
+++ Project Portfolio/Project 2/Portfolio subproject 2/Exercise4_3/wwwroot/js/components/post/post.js
@@ -0,0 +1,17 @@
+﻿define(['knockout', 'store'], (ko, store) => {
+    return function (params) {
+        var currentPost = ko.observable(store.getState().selectedPost);
+        
+        var home = () => {
+            store.dispatch(store.actions.pageListTitle());
+            store.dispatch(store.actions.pageListView());
+        };
+
+        store.dispatch(store.actions.changeTitle("Post"));
+
+        return {
+            currentPost,
+            home
+        };
+    }
+});
