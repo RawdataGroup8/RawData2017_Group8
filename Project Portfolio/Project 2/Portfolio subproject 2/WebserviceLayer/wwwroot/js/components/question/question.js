3d63255e81852204d18a496be87f104dd82c107a
Rune Barrett <runebarrett@gmail.com>
2017-12-17 20:05:36 +0100

    add question

---
+++ Project Portfolio/Project 2/Portfolio subproject 2/WebserviceLayer/wwwroot/js/components/question/question.js
@@ -0,0 +1,57 @@
+﻿define(['knockout', 'store'], (ko, store) => {
+    return function (params) {
+        var currentPost = ko.observable(store.getState().selectedPost);
+        var koRocks = ko.observable(false);
+        var home = () => {
+            store.dispatch(store.actions.pageListTitle());
+            store.dispatch(store.actions.pageListView());
+        };
+
+        store.dispatch(store.actions.changeTitle("Post"));
+
+        return {
+            currentPost,
+            home,
+            koRocks
+        };
+    }
+});
+
+/*require(['jquery', 'knockout'], ($, ko) => {
+
+
+        var title = ko.observable("Show Question");
+      
+        var currentPost = ko.observable();
+
+        var showPost = (data) => {
+            $.getJSON(data.link, postData => {
+                var post = {
+                    title: postData.title,
+                    score: postData.score,
+                    creationDate: postData.creationDate,
+                    body: postData.body
+                }
+
+                $.getJSON(postData.answers, ans => {
+                    post.answers = ko.observableArray(ans);
+                    currentPost(post);
+                });
+            });
+            title("Post");
+            //currentView('postview');
+        };
+
+        var home = () => {
+            title("Show Posts");
+            //currentView('postlist');
+        };
+
+        return {
+            title,
+            //currentView,
+            showPost,
+            currentPost,
+            home
+        };
+});*/
