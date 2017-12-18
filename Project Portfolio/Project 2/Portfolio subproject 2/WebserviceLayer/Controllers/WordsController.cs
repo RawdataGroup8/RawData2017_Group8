0587ee97b793a79d742e236b409e0f35b841374d
Rune Barrett <runebarrett@gmail.com>
2017-12-18 10:58:54 +0100

    add ranked words api

---
+++ Project Portfolio/Project 2/Portfolio subproject 2/WebserviceLayer/Controllers/WordsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccesLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLayer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/words")]
+    public class WordsController : Controller
+    {
+        private readonly IDataService _ds;
+        public WordsController(IDataService iDataService) => _ds = iDataService;
+
+
+        //GET: api/search/java%20python
+        [HttpGet("{terms}", Name = nameof(RankedWordsSearch))]       
+        public IActionResult RankedWordsSearch(string terms, int page = 0, int pageSize = 10)
+        {
+            var data = _ds.RankedWordsSearch(terms, page, pageSize).Select(x => new
+            {
+                //Link = Url.Link(nameof(PostsController.GetPost), new { x.PostId }),
+                //x.
+                word = x.Word,
+                rank = x.Rank
+
+            });
+            /*var total = 100;//need to find a way to get the length of the IEnumerable here
+            var pages = Math.Ceiling(total / (double)pageSize);
+            var prev = page > 0 ? Url.Link(nameof(RankedWordsSearch), new { page = page - 1, pageSize }) : null;
+            var next = page < pages - 1 ? Url.Link(nameof(RankedWordsSearch), new { page = page + 1, pageSize }) : null;
+
+            var result = new
+            {
+                total,
+                pages,
+                prev,
+                next,
+                items = data
+            };*/
+            return Ok(data);
+        }
+
+    }
+}
